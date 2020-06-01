66b588819c4e3efe15c5a9b00c10c44768919169
phoenixSP <pal00007@umn.edu>
2020-06-01 17:30:46 -0500

    adding errorhandling

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ Created on Tue May 26 21:51:05 2020
 
 from flask import Flask, render_template, request,session,redirect,url_for, jsonify, abort
 from flask_socketio import SocketIO, emit
+from utils.calculator import evaluate
 
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
@@ -46,18 +47,24 @@ def getHistory():
     try:
         return (jsonify({'history':history}), 200)
     except:
-        abort(404)
+        abort(400)
 
 @app.route('/', methods= ['POST', 'GET'])
 def getCalculation():
     if request.method == 'POST':
-        operation = request.form['operation']
-        if getCalculationFunction(operation):
-            addOperation(operation, getCalculationFunction(operation))
-            print(history)
+        try:
+            operation = request.form['operation']
+#            if getCalculationFunction(operation):
+#                addOperation(operation, getCalculationFunction(operation))
+#                print(history)
+#                return render_template("index.html", result= getCalculationFunction(operation))
+            result = evaluate(operation)
+            addOperation(operation, result)
             return render_template("index.html", result= getCalculationFunction(operation))
-        else:
-            abort(404)
+        except Exception as err:
+            return "Error encountered: {}".format(err)
+#        else:
+#            abort(400)
         #return "User {} calculated {}".format(session['username'], getCalculationFunction(operation))
     else:
         return render_template("index.html")
